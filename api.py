aae18ea7f14ef7aae4faf4c5192cc8be17604a79
Seth Braddock <sethbrad@iastate.edu>
2020-12-03 12:05:41 -0600

    attempting trades

--- api.py
+++ api.py
@@ -1,19 +1,24 @@
-import cbpro, json, base64
+import cbpro, json
 
 with open("secret.json") as json_file:
     secret = json.load(json_file)
 
 authClient = cbpro.AuthenticatedClient(secret["apiKey"], secret["apiSecret"], secret["passphrase"], api_url="https://api.pro.coinbase.com")
 
+with open("xrp.json") as json_file:
+    xrp = json.load(json_file)
+
 # API functions
-def getBalances():
-    return 0
+def getAcc(): # id set to XRP account 
+    return authClient.get_account(xrp["id"])
 
 def getPrices():
     return 0
 
-def buy():
-    return 0
+def buy(funds):
+    authClient.place_market_order(product_id="XRP", side="buy", funds=funds)
+    orderList = authClient.get_orders()
+    print(list(orderList))
 
 def sell():
     return 0
--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import time
+import time, api
 
 # Bot has BUYING and SELLING state
 # Access transactions and data through Coinbase
@@ -36,19 +36,24 @@ def trade():
         attemptSell()
 
 def attemptBuy():
-    percentDiff = (getPrices() - lastPrice) / lastPrice * 100.0
+    percentDiff = (api.getPrices() - lastPrice) / lastPrice * 100.0
     if(percentDiff > DIP_TH or percentDiff > UPWARD_TH):
-        buy()
+        api.buy()
         state = not state
         log("Buy completed.\n")
 
 def attemptSell():
-    percentDiff = (getPrices() - lastPrice) / lastPrice * 100.0
+    percentDiff = (api.getPrices() - lastPrice) / lastPrice * 100.0
     if(percentDiff > PROFIT_TH or percentDiff < DOWNWARD_TH):
-        sell()
+        api.sell()
         state = not state
         log("Sell completed.\n")
 
 def log(msg):
     print(msg)
-    logFile.write(msg) # add time stamps
+    print(time.asctime())
+
+    logFile.write(msg)
+    logFile.write(time.asctime())
+
+# api.buy(1.00)
